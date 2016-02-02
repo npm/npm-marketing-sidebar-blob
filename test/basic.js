736def0e8200140c72e197362955f5a5f8103005
Jaap Vossers <jvossers@gmail.com>
2016-02-02 20:06:35 +0000

    fixed orgainzations typo (another one)

--- test/basic.js
+++ test/basic.js
@@ -13,6 +13,7 @@ for (var i = 0; i < blobs.length; i++) {
   t.notMatch(blobs[i], /[^&]nbsp;/)
   t.notMatch(blobs[i], /[^&]nbsp[^;]/)
   t.notMatch(blobs[i], 'Organiztions')
+  t.notMatch(blobs[i], 'Orgainzations')
   t.match(blobs[i], /^<h3[ >]/)
   t.match(blobs[i], /<a /)
 }
